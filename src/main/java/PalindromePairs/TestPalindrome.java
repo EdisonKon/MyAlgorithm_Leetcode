bb7560d3311475a5e874e553f2848268e1fb4f9d
dekai.kong <dekai.kong@luckincoffee.com>
2018-11-12 13:53:58 +0800

    group

--- src/main/java/PalindromePairs/TestPalindrome.java
+++ src/main/java/PalindromePairs/TestPalindrome.java
@@ -8,11 +8,23 @@ import java.util.*;
  * @date: 2018-10-24 15:12
  */
 
-
+/***
+ * //测试回文
+ *     //https://leetcode.com/problems/palindrome-pairs/description/
+ * Given a list of unique words, find all pairs of distinct indices (i, j) in the given list, so that the concatenation of the two words, i.e. words[i] + words[j] is a palindrome.
+ *
+ * Example 1:
+ *
+ * Input: ["abcd","dcba","lls","s","sssll"]
+ * Output: [[0,1],[1,0],[3,2],[2,4]]
+ * Explanation: The palindromes are ["dcbaabcd","abcddcba","slls","llssssll"]
+ * Example 2:
+ *
+ * Input: ["bat","tab","cat"]
+ * Output: [[0,1],[1,0]]
+ * Explanation: The palindromes are ["battab","tabbat"]
+ * */
 public class TestPalindrome {
-    //测试回文
-    //https://leetcode.com/problems/palindrome-pairs/description/
-
     public static void main(String[] args) {
         String arr[] = {"abcd","dcba","lls","s","sssll"};
 //        String arr[] = {"bat","tab","cat"};
