6470f14f54636550e44bf953a902f603b54dbfeb
dekai.kong <dekai.kong@luckincoffee.com>
2018-11-12 16:47:02 +0800

    longest palindrome string

---
+++ src/main/java/PalindromePairs/LongestPalindromeString.java
@@ -0,0 +1,32 @@
+package PalindromePairs;
+
+/**
+ * @description: 描述
+ * @author: dekai.kong (dekai.kong@luckincoffee.com)
+ * @date: 2018-11-12 16:46
+ *
+ *
+ * 5. Longest Palindromic Substring
+ * Medium
+ * 2529
+ * 241
+ *
+ *
+ * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+ *
+ * Example 1:
+ *
+ * Input: "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ * Example 2:
+ *
+ * Input: "cbbd"
+ * Output: "bb"
+ */
+
+
+public class LongestPalindromeString {
+
+
+}
