dc5ba0b53109a9e5284a31324ea4533194d9434c
dekai.kong <dekai.kong@luckincoffee.com>
2018-11-12 19:10:50 +0800

    test

--- src/main/java/PalindromePairs/LongestPalindromeString.java
+++ src/main/java/PalindromePairs/LongestPalindromeString.java
@@ -68,8 +68,32 @@ public class LongestPalindromeString {
         }
         return mapKv.get(maxLen);
     }
+
+    public String longestPalindrome2(String s) {
+        int len = s.length();
+        int left = 0, right = len - 1;
+        if(s.length() == 0 || s.length() == 1 || isBack(s,left,right)){
+            return s;
+        }
+        for(int i = 1;i<len;i++){
+            for(int j = 0;j<=i;j++){
+                String rs = s.substring(j,len-i+j);
+                if(isBack(rs,0,rs.length()-1)){
+                    return rs;
+                }
+            }
+        }
+        return "";
+    }
+    public boolean isBack(String s,int left,int right){
+        while(left < right){
+            if(s.charAt(left++) != s.charAt(right--)) return false;
+        }
+        return true;
+    }
     @Test
     public void TestLongestPalindromeString(){
-        System.out.println(longestPalindrome("bbbb"));
+//        System.out.println(longestPalindrome("bbbb"));
+        System.out.println(longestPalindrome2("babad"));
     }
 }
