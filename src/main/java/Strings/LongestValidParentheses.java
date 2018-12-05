17759652d05d044fa1eb55b66a690df0b8167d67
dekai.kong <dekai.kong@luckincoffee.com>
2018-12-05 11:46:51 +0800

    leetcode 32 hard

--- src/main/java/Strings/LongestValidParentheses.java
+++ src/main/java/Strings/LongestValidParentheses.java
@@ -28,119 +28,124 @@ public class LongestValidParentheses {
 
     }
 
-    //)())(())()
+    /**
+     * Runtime: 12 ms, faster than 80.14% of Java online submissions for Longest Valid Parentheses.
+     * @param s
+     * @return
+     * 从头遍历取每一个()向两边扫描,如果左右形成(),那就当前的index是最右边的index(i),
+     * 如果不能形成,判断左边index(j)值在map里有没有,如果有,那就加上该值len,将j-len继续向左搜索直道无法搜索,存入map(i,len)
+     * i 取最后值的++值,继续遍历
+     */
     public int longestValidParentheses(String s) {
-        int rst = 0;
-        int len = 0;
-        int left = 0;
-        int right = 0;
+        int rst = 0; int left = 0; int right = 0;
         Map<Integer,Integer> mapIL = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
             if('(' == s.charAt(i)){
-                if (left==0)left = 1;
+                if (left==0){left = 1;}
             }else{
-                if(left == 1)right = 1;
+                if(left == 1){right = 1;}
             }
-            if(left == right){
-                len = 1;
+            if(left == right && left==1){
+                int len = 1;
                 left = right = 0;
                 int j = i-2;//左边的左边
-                int k = i+1;//当前位置的右边
-                while(j>0 && k<s.length()){
-                    if(s.charAt(j)=='(' && s.charAt(k)==')'){
-                        len++;
-                        j--;
-                        k++;
-                    }else if(s.charAt(j)==')'){
+                while(j>=0 && i+1<=s.length()){
+                    if(i+1!=s.length() && s.charAt(j)=='(' && s.charAt(i+1)==')'){
+                        len++; j--; i++;
+                    }else{
                         int val = mapIL.getOrDefault(j,-1);
                         if( val != -1){
-                            j -= val * 2;
+                            j -= (val * 2);
                             len += val;
-                        }else{
-                            mapIL.put(k,len);
-                            break;
-                        }
+                        }else{ break; }
                     }
                 }
-                mapIL.put(k,len);
+                mapIL.put(i,len);
+                rst = Math.max(rst,len);
             }
         }
-        return rst;
+        return rst*2;
     }
 
-    public int longestValidParentheses2(String s) {
-        int left = 0;
-        int right = 0;
-        int rst = 0;
-        int curr = 0;
-        int newrst = 0;
-        int lastindex = Integer.MIN_VALUE;
-        int curindex = 0;
-        boolean isnew = true;
-        String rs = "";
+    /**
+     * map 换成 array 的解法
+     * Runtime: 12 ms, faster than 80.14% of Java online submissions for Longest Valid Parentheses.
+     * @param s
+     * @return
+     */
+    public int longestValidParentheses3(String s) {
+        int rst = 0; int left = 0; int right = 0;
+        int[] mapIL = new int[s.length()];
         for (int i = 0; i < s.length(); i++) {
             if('(' == s.charAt(i)){
-                if(curindex == 0){
-                    curindex = i;
-                }
-                left += 1;
-                rs+="(";
-                curr = Math.min(left,right);
+                if (left==0){left = 1;}
             }else{
-                if(left == right && left ==0 ){
-                    left = right = 0;
-                    continue;
-                }
-                right += 1;
-                rs+=")";
-                curr = Math.min(left,right);
+                if(left == 1){right = 1;}
             }
-
-            if(left == right && left!=0){
+            if(left == right && left==1){
+                int len = 1;
                 left = right = 0;
-                if (lastindex+1 == curindex){
-                    if(!isnew){
-                        rst = Math.max(rst+curr,curr);
+                int j = i-2;
+                while(j>=0 && i+1<=s.length()){
+                    if(i+1!=s.length() && s.charAt(j)=='(' && s.charAt(i+1)==')'){
+                        len++; j--; i++;
                     }else{
-                        rst = Math.max(newrst+curr,rst);
-                        isnew = false;
+                        int val = mapIL[j];
+                        if( val != 0){
+                            j -= (val * 2);
+                            len += val;
+                        }else{ break; }
                     }
-                }else{
-                    newrst = 0;
-                    rst = Math.max(rst,curr);
-                    newrst += curr;
-                    isnew = true;
                 }
-                curindex = 0;
-                lastindex = i;
+                mapIL[i] = len;
+                rst = Math.max(rst,len);
             }
-
-//            if(left>right && i == s.length()-1){
-//                curr = Math.min(left,right);
-//                curindex = i - curr;
-//                lastindex = i;
-//            }
-//            if (lastindex+1 == curindex){
-//                rst = Math.max(rst+curr,curr);
-//            }
         }
-        rst = Math.max(rst,curr);
-//        System.out.println(rs);
-        System.out.println(rst);
         return rst*2;
+    }
+/**
+ * 8ms znm解法
+ * 为每一个)去找前面完整的(),然后再往前找pre的index,如果有值就相加
+ * 比如()(())
+ * 从第一个开始是因为无论第0个是( 还是 ) 都不影响,如果第一个是 ( 继续跳过,如果是)则进入
+ * 获取previndex,根据前一个的长度往前找,第一次是0,那就是前面还没有完整的()
+ * 如果previndex超出左边范围或者它不是( 完成不了一个完整的() 那就跳出
+ * 否则可以完成,肯定是( 所以这个index的值+2是一个完整的(),对比max
+ * 第二次...推理
+ */
+
+    public int longestValidParentheses2(String s) {
+        int dp[] = new int[s.length()];
+        int result = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if(s.charAt(i) == '('){
+                continue;
+            }
+            int prev = i - dp[i-1] -1;
+            if(prev < 0 || s.charAt(prev)!='('){
+                continue;
+            }
+            dp[i] = dp[i-1] + 2;
+            if(prev > 0){
+                dp[i] += dp[prev -1];
+            }
+            result = Math.max(result,dp[i]);
+        }
+        return result;
     }
 
     @Test
     public void test() {
-//        longestValidParentheses("(()");
+//        longestValidParentheses2("(()");
 //        longestValidParentheses("()(()");
 //        longestValidParentheses(")()())");
 //        longestValidParentheses(")((()())");
 //        longestValidParentheses(")((())))))");
 //        longestValidParentheses(")(()))()");
-//        longestValidParentheses("()(())");
+        longestValidParentheses3("()(())");
 //        longestValidParentheses(")()())()()(");
-        longestValidParentheses("(()(((()");
-        // 1 1 2 3 3 2 3 2 1
+//        longestValidParentheses("(()(((()");
+//        longestValidParentheses(")()(())()");
+        // 1 1 2 3 3 2 3 2 1 3
     }
 }
