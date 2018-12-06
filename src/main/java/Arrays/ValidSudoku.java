efd29dbe24470b4dec0e32be97fb4d086f22eae7
dekai.kong <dekai.kong@luckincoffee.com>
2018-12-06 17:20:33 +0800

    leetcode 36 Medium

---
+++ src/main/java/Arrays/ValidSudoku.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+import org.junit.Test;
+
+/**
+ * @description: 描述 Medium
+ * @author: dekai.kong
+ * @date: 2018-12-06 16:25
+ * @from https://leetcode.com/problems/valid-sudoku/
+ *
+ */
+
+public class ValidSudoku {
+    public ValidSudoku() {
+
+    }
+
+    public boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+
+            }
+        }
+        return false;
+    }
+
+    @Test
+    public void test() {
+    }
+}
+
