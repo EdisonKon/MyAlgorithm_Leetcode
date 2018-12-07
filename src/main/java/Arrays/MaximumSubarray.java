716debc2698926881c43bbd00ac8a9f571fdc3b4
dekai.kong <dekai.kong@luckincoffee.com>
2018-12-07 11:27:58 +0800

    leetcode 53 Easy

--- src/main/java/Arrays/MaximumSubarray.java
+++ src/main/java/Arrays/MaximumSubarray.java
@@ -24,11 +24,31 @@ public class MaximumSubarray {
     }
 
     /**
-     * Runtime: 7 ms, faster than 90.91% of Java online submissions for Maximum Subarray.
+     * Runtime: 6 ms, faster than 99.94% of Java online submissions for Maximum Subarray.
      * @param nums
      * @return
      */
     public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(sum < 0){
+                sum = nums[i];
+            }else{
+                sum += nums[i];
+            }
+            if(max <sum){
+                max = sum;
+            }
+        }
+        return max;
+    }
+    /**
+     * Runtime: 7 ms, faster than 90.91% of Java online submissions for Maximum Subarray.
+     * @param nums
+     * @return
+     */
+    public int maxSubArray2(int[] nums) {
         int curMAx = Integer.MIN_VALUE;
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
@@ -44,7 +64,9 @@ public class MaximumSubarray {
 
     @Test
     public void test(){
-        int[] a = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+//        int[] a = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+        int[] a = new int[]{-5,-6,-7,7};
         maxSubArray(a);
+        maxSubArray2(a);
     }
 }
