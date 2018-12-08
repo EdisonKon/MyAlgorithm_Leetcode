b99d33786115b1a31d850809ff1c23a67ea79dbe
dekai.kong <dekai.kong@luckincoffee.com>
2018-12-08 18:18:24 +0800

    leetcode 43 Medium

--- src/main/java/Strings/MultiplyStrings.java
+++ src/main/java/Strings/MultiplyStrings.java
@@ -1,5 +1,6 @@
 package Strings;
 
+import com.sun.deploy.util.StringUtils;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -50,16 +51,56 @@ public class MultiplyStrings {
         Map<Character,List<Integer>> mapCi = new HashMap<>(10);
         List<Integer> nums = new ArrayList<>();//存结果
         int isUp = 0;
-        for (int i = num2.length()-1; i > 0; i++) {
-            for (int j = num1.length()-1; j > 0; j++) {
-                num1[i]
+        for (int i = num2.length()-1; i >= 0; i--) {
+            if(num2.charAt(i) == '0'){continue;}//排出0的情况
+            List<Integer> mulend = new ArrayList<>();
+            List<Integer> mulval = new ArrayList<>();
+            if(mapCi.getOrDefault(num2.charAt(i),null) !=null){//map里有记录了
+                mulval = mapCi.get(num2.charAt(i));
+            }else{//map里没有记录,重新相乘
+                for (int j = num1.length()-1; j >= 0; j--) {
+                    int val = Character.getNumericValue(num2.charAt(i))*Character.getNumericValue(num1.charAt(j)) + isUp;
+                    isUp = val/10;
+                    mulval.add(0,val%10);
+                }
+                mapCi.put(num2.charAt(i),mulval);
+            }
+            mulend.addAll(mulval);
+            for (int j = 0; j < num2.length()-1 - i; j++) {
+                mulend.add(0);//给结果补0
+            }
+            int len = nums.size();
+            int lenm = mulend.size();
+            if (len >0){
+                int addUp = 0;
+                for (int j = lenm-1; j >= 0; j--) {
+                    int index = j-(lenm - len);
+                    if(index<0){
+                        nums.add(0,mulend.get(j)+addUp);
+                    }else {
+                        int val = nums.get(j-(lenm - len)) + mulend.get(j)+addUp;
+                        nums.set(j-(lenm - len),val%10);
+                        addUp = val/10;
+                    }
+                }
+            }else{
+                nums.addAll(mulend);
             }
         }
+        if(nums.size() == 0){
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nums.size(); i++) {
+            sb.append(nums.get(i));
+        }
+        return sb.toString();
     }
 
     @Test
     public void test() {
-
+//        System.out.println(multiply("123","456"));
+        System.out.println(multiply("9","9"));
     }
 }
 
