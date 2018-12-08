0dd04762040f880019a1d2eb590f84c329144b50
dekai.kong <dekai.kong@luckincoffee.com>
2018-12-08 16:09:24 +0800

    leetcode 39/40 Medium

--- src/main/java/Arrays/CombinationSum.java
+++ src/main/java/Arrays/CombinationSum.java
@@ -36,16 +36,93 @@ import java.util.*;
  *   [2,3,3],
  *   [3,5]
  * ]
+ * 讲解
+ * https://www.youtube.com/watch?v=aBL-aNWNmB4&t=23s
+ * TODO 多刷(经典递归)
  */
 
 public class CombinationSum {
     public CombinationSum() {
 
     }
+
+    /**
+     * Runtime: 10 ms, faster than 82.80% of Java online submissions for Combination Sum.
+     * @param candidates
+     * @param target
+     * @return
+     */
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> rst = new ArrayList<>();
+        if(candidates.length != 0){
+            Arrays.sort(candidates);
+            List<Integer> curList = new ArrayList<>();
+            doRec(candidates,target,0,rst,curList);
+        }
         return rst;
     }
+    public List<List<Integer>> doRec(int[] candidates, int target,int index, List<List<Integer>> rst, List<Integer> curList){
+        if(target == 0){
+            rst.add(new ArrayList<>(curList));
+        }else if(target>0){
+            for (int i = index; i < candidates.length; i++) {
+                if(candidates[i] > target) continue;
+                curList.add(candidates[i]);
+                doRec(candidates,target-candidates[i],i,rst,curList);
+                curList.remove(curList.size()-1);
+            }
+        }
+        return rst;
+    }
+
+    /**
+     * fail
+     * @param candidates
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> combinationSum3(int[] candidates, int target) {
+        List<List<Integer>> rst = new ArrayList<>();
+        if(candidates.length != 0){
+            Arrays.sort(candidates);
+            for (int i = 0; i < candidates.length; i++) {
+                rst.addAll(doRecursive(target,i,candidates));
+            }
+        }
+        return rst;
+    }
+    public List<List<Integer>> doRecursive(int target,int curinex,int[] candidates){
+        List<List<Integer>> inrst = new ArrayList<>();
+        if (candidates[curinex] > target){
+            return inrst;
+        }
+        List<Integer> lin = new ArrayList<>();//初始化一个-1的 用以记录每一个candidates[curinex],并且递减
+        for (int i = 0; i < target/candidates[curinex] ; i++) {
+            lin.add(candidates[curinex]);
+        }
+        while(lin.size()>0){
+            int val = 0;
+            for (int i = 0; i < lin.size(); i++) {
+                val+= lin.get(i);
+            }
+            List<Integer> inlin = new ArrayList();
+            int inval = val;
+            inlin.addAll(lin);
+            for (int i = curinex+1; i < candidates.length; i++) {
+                while(inval < target){
+                    inval += candidates[i];
+                    inlin.add(candidates[i]);
+                }
+                if(inval == target){
+                    inrst.add(inlin);
+                    break;
+                }
+            }
+            lin.remove(lin.size()-1);
+        }
+        return inrst;
+    }
+
 
 
     /**
@@ -55,52 +132,55 @@ public class CombinationSum {
      * @return
      */
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Map<Integer,List<List<Integer>>> mapkv = new HashMap<>();
-        Set<String> boo= new HashSet<>();
+        Set<Integer> set = new HashSet();
+        for (int i = 0; i < candidates.length; i++) {
+            set.add(candidates[i]);
+        }
+        Map<Integer,Integer> mapuse = new HashMap<>();
         List<List<Integer>> rst = new ArrayList<>();
         for (int i = 0; i < candidates.length; i++) {
             int cur = candidates[i];
             List<Integer> add = new ArrayList<>();
-            doCheck(cur,add,rst,target,boo,mapkv);
+            doCheck(target,cur,add,set,rst,target,mapuse);
         }
         return rst;
 
     }
-    public void doCheck(int cur,List<Integer> add,List<List<Integer>> rst,int selftar,Set<String> boo,Map<Integer,List<List<Integer>>> mapkv){
-        int val = 0;
-        for (int j = 0; j < selftar / cur; j++) {
-            val += cur;
-            add.add(cur);
-            int t_v = selftar-val;
-            if(t_v<=0){
-                break;
-            }
-            if(mapkv.getOrDefault(t_v,null)!=null){
-                List<Integer> addin = new ArrayList<>();
-                addin.addAll(add);
-                addin.add(t_v);
-                Collections.sort(addin);
-                if(!boo.add(addin.toString())){
-                    continue;
-                }
-                rst.add(addin);
-            }else{
-                List x = new ArrayList<>();
-                x.addAll(add);
-                doCheck(cur,x,rst,t_v,boo,mapkv);
-                for (int i = 0; i < rst.size(); i++) {
-                    rst.get(i).addAll(add);
+    public void doCheck(int target,int cur,List<Integer> add, Set<Integer> set,List<List<Integer>> rst,int selftar,Map<Integer,Integer> mapuse){
+        if(selftar % cur== 0 ){
+            int val = 0;
+            for (int j = 0; j < target / cur; j++) {
+                val += cur;
+                add.add(cur);
+                int t_v = target-val;
+                if(set.contains(t_v) && t_v!=cur){
+                    List<Integer> addin = new ArrayList<>();
+                    addin.addAll(add);
+                    if(mapuse.getOrDefault(t_v,-1)!=-1){
+                        continue;
+                    }
+                    addin.add(t_v);
+                    if(addin.size() == 2){
+                        mapuse.put(cur,1);
+                    }
+                    rst.add(addin);
                 }
             }
+            if(selftar==target){
+                rst.add(add);
+            }
+        }else{
+            doCheck(target,cur,add,set,rst,target - target%cur,mapuse);
         }
     }
 
     @Test
     public void test() {
+//        int[] s = new int[]{2,3,5};int target = 7;
 //        int[] s = new int[]{2,3,6,7};int target = 7;
         int[] s = new int[]{2,3,7};int target = 18;
 //        int[] s = new int[]{1,2,3,4,5};int target = 7;
-        combinationSum2(s,target);
+        combinationSum(s,target);
     }
 }
 
---
+++ src/main/java/Arrays/CombinationSumII.java
@@ -0,0 +1,88 @@
+package Arrays;
+
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * @description: 描述 Medium
+ * @author: dekai.kong
+ * @date: 2018-12-07 11:29
+ * @from https://leetcode.com/problems/combination-sum/
+ *
+ * Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
+ *
+ * Each number in candidates may only be used once in the combination.
+ *
+ * Note:
+ *
+ * All numbers (including target) will be positive integers.
+ * The solution set must not contain duplicate combinations.
+ * Example 1:
+ *
+ * Input: candidates = [10,1,2,7,6,1,5], target = 8,
+ * A solution set is:
+ * [
+ *   [1, 7],
+ *   [1, 2, 5],
+ *   [2, 6],
+ *   [1, 1, 6]
+ * ]
+ * Example 2:
+ *
+ * Input: candidates = [2,5,2,1,2], target = 5,
+ * A solution set is:
+ * [
+ *   [1,2,2],
+ *   [5]
+ * ]
+ * 有重复的,需要进行消重
+ * 讲解
+ * https://www.youtube.com/watch?v=aBL-aNWNmB4&t=23s
+ * TODO 多刷(经典递归)
+ */
+
+public class CombinationSumII {
+    public CombinationSumII() {
+
+    }
+
+    /**
+     * Runtime: 8 ms, faster than 99.15% of Java online submissions for Combination Sum II.
+     * @param candidates
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> rst = new ArrayList<>();
+        if(candidates.length != 0){
+            Arrays.sort(candidates);
+            List<Integer> curList = new ArrayList<>();
+            doRec(candidates,target,0,rst,curList);
+        }
+        return rst;
+    }
+    public List<List<Integer>> doRec(int[] candidates, int target,int index, List<List<Integer>> rst, List<Integer> curList){
+        if(target == 0){
+            rst.add(new ArrayList<>(curList));
+        }else if(target>0){
+            for (int i = index; i < candidates.length; i++) {
+                if(candidates[i] > target) {continue;}
+                if(index!=i && candidates[i] == candidates[i-1]) {continue;} //消重操作
+                curList.add(candidates[i]);
+                doRec(candidates,target-candidates[i],i+1,rst,curList);
+                curList.remove(curList.size()-1);
+            }
+        }
+        return rst;
+    }
+
+
+    @Test
+    public void test() {
+        int[] s = new int[]{10,1,2,7,6,1,5};int target = 8;
+//        int[] s = new int[]{2,5,2,1,2};int target = 5;
+        combinationSum2(s,target);
+    }
+}
+
