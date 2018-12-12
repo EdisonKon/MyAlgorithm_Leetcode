7dcdae7c78bba475f00f5952a2f9ed409e5f9541
dekai.kong <dekai.kong@luckincoffee.com>
2018-12-12 17:26:26 +0800

    leetcode 42 hard

---
+++ src/main/java/Integers/TrappingRainWater.java
@@ -0,0 +1,36 @@
+package Integers;
+
+import org.junit.Test;
+
+/**
+ * @description: 描述 Hard
+ * @author: dekai.kong
+ * @date: 2018-12-12 17:25
+ * @from https://leetcode.com/problems/trapping-rain-water/
+ * Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+ *
+ *
+ * The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+ * Thanks Marcos for contributing this image!
+ *
+ * Example:
+ *
+ * Input: [0,1,0,2,1,0,1,3,2,1,2,1]
+ * Output: 6
+ */
+
+public class TrappingRainWater {
+    public TrappingRainWater() {
+
+    }
+
+    public int trap(int[] height) {
+        return 0;
+    }
+
+    @Test
+    public void test() {
+
+    }
+}
+
