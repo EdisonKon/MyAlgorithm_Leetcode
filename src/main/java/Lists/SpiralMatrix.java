90380e15cec742b7155a65c4d22eef93146745e5
dekai.kong <dekai.kong@luckincoffee.com>
2018-12-13 10:55:30 +0800

    leetcode 58 Easy

---
+++ src/main/java/Lists/SpiralMatrix.java
@@ -0,0 +1,31 @@
+package Lists;
+
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @description: 描述 Medium
+ * @author: dekai.kong
+ * @date: 2018-12-13 10:53
+ * @from
+ */
+
+public class SpiralMatrix {
+    public SpiralMatrix() {
+
+    }
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+
+    }
+
+    @Test
+    public void test() {
+        spiralOrder(new int[][]{ {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        });
+    }
+}
+
--- src/main/java/Strings/LengthofLastWord.java
+++ src/main/java/Strings/LengthofLastWord.java
@@ -6,7 +6,17 @@ import org.junit.Test;
  * @description: 描述 Easy
  * @author: dekai.kong
  * @date: 2018-12-13 10:37
- * @from
+ * @from https://leetcode.com/problems/length-of-last-word/
+ * Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+ *
+ * If the last word does not exist, return 0.
+ *
+ * Note: A word is defined as a character sequence consists of non-space characters only.
+ *
+ * Example:
+ *
+ * Input: "Hello World"
+ * Output: 5
  */
 
 public class LengthofLastWord {
