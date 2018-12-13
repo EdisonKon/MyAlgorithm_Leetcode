753d7a72402fc0cf7f1a29e1cf1125a1b431ae7d
dekai.kong <dekai.kong@luckincoffee.com>
2018-12-13 15:05:39 +0800

    leetcode 56 medium

--- src/main/java/Lists/MergeIntervals.java
+++ src/main/java/Lists/MergeIntervals.java
@@ -1,12 +1,30 @@
 package Lists;
 
+import Entitys.Interval;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * @description: 描述 Medium
  * @author: dekai.kong
  * @date: 2018-12-13 14:49
- * @from
+ * @from https://leetcode.com/problems/merge-intervals/
+ * Given a collection of intervals, merge all overlapping intervals.
+ *
+ * Example 1:
+ *
+ * Input: [[1,3],[2,6],[8,10],[15,18]]
+ * Output: [[1,6],[8,10],[15,18]]
+ * Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+ * Example 2:
+ *
+ * Input: [[1,4],[4,5]]
+ * Output: [[1,5]]
+ * Explanation: Intervals [1,4] and [4,5] are considered overlapping.
  */
 
 public class MergeIntervals {
@@ -15,11 +33,26 @@ public class MergeIntervals {
     }
 
     public List<Interval> merge(List<Interval> intervals) {
+        List<Interval> list = new ArrayList<>();
+        Collections.sort(intervals,new IntervalsComp());
+
+        return list;
+    }
+    class IntervalsComp implements Comparator<Interval>{
+        @Override
+        public int compare(Interval o1, Interval o2) {
+            return o1.start - o2.start;
+        }
     }
 
     @Test
     public void test() {
-
+        List<Interval> list = new ArrayList<>();
+        list.add(new Interval(1,3));
+        list.add(new Interval(2,6));
+        list.add(new Interval(8,10));
+        list.add(new Interval(15,18));
+        merge(list);
     }
 }
 
