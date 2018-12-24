9cefdf3a628df7c946137ce0f7d5cad297d9bbda
dekai.kong <huainian_cl@126.com>
2018-12-24 20:47:37 +0800

    leetcode 79 medium

--- src/main/java/Booleans/WordSearch.java
+++ src/main/java/Booleans/WordSearch.java
@@ -31,21 +31,73 @@ public class WordSearch {
 
     }
 
+    /**
+     * Runtime: 21 ms, faster than 20.53% of Java online submissions for Word Search.
+     * @param board
+     * @param word
+     * @return
+     */
     public boolean exist(char[][] board, String word) {
         if(board==null||board.length==0||board[0].length==0){
             return false;
         }
         //用于记录是否用过
-        int[][] copy = new int[board.length][board[0].length];
+        int olen = board.length;
+        int ilen = board[0].length;
+        int[][] copy = new int[olen][ilen];
+        char[] words = word.toCharArray();
+        int ix = 0;
+        for (int i = 0; i < olen; i++) {
+            for (int j = 0; j < ilen; j++) {
+                if(board[i][j] == words[ix]){
+                    if(helper(i,j,olen,ilen,words,ix,copy,board)){
+                        return true;
+                    }
+                    copy = new int[olen][ilen];
+                    ix = 0;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean helper(int i,int j,int olen,int ilen,char[] words,int index,int[][] copy,char[][] board){
+        if(index == words.length){
+            return true;
+        }
+        if(i<0 || i>olen-1 || j<0 || j > ilen-1){
+            return false;
+        }
+        if(words[index] == board[i][j] && copy[i][j] != 1){
+            copy[i][j] = 1;
+            if(i>=0 || i<=olen-1 || j>=0 || j<= ilen-1){
+                boolean bo = helper(i-1,j,olen,ilen,words,index+1,copy,board)||
+                        helper(i,j-1,olen,ilen,words,index+1,copy,board)||
+                        helper(i+1,j,olen,ilen,words,index+1,copy,board)||
+                        helper(i,j+1,olen,ilen,words,index+1,copy,board);
+                if(!bo){
+                    copy[i][j] = 0;
+                }
+                return bo;
+
+            }
+        }else{
+            return false;
+        }
         return false;
     }
 
     @Test
     public void test() {
-        char[][] board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
-        System.out.println(exist(board,"ABCCED"));
-        System.out.println(exist(board,"SEE"));
-        System.out.println(exist(board,"ABCB"));
+        char[][] board = {{'A','B','C','E'},{'S','F','E','S'},{'A','D','E','E'}};
+//        char[][] board = {{'A'}};
+//        System.out.println(exist(board,"A"));
+//        System.out.println(exist(board,"ABCCED"));
+//        System.out.println(exist(board,"SEE"));
+//        System.out.println(exist(board,"ABCB"));
+        System.out.println(exist(board,"ABCESEEEFS"));
+
     }
 }
 
