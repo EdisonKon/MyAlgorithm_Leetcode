464365d3ca6f27712262bdeb2f6cf901694c60dd
dekai.kong <huainian_cl@126.com>
2018-12-30 18:19:22 +0800

    leetcode 89 medium

---
+++ src/main/java/Lists/GrayCode.java
@@ -0,0 +1,25 @@
+package Lists;
+
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @author dekai.kong
+ * @difficult Medium
+ * @create 2018-12-30 18:18
+ * @from
+ **/
+public class GrayCode {
+    public GrayCode() {
+
+    }
+
+    public List<Integer> grayCode(int n) {
+    }
+
+    @Test
+    public void test() {
+
+    }
+}
