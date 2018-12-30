e603d4ac2128a49ab126758bbf6a54d814c99669
dekai.kong <huainian_cl@126.com>
2018-12-30 19:07:10 +0800

    leetcode 90 medium

--- src/main/java/Lists/SubsetsII.java
+++ src/main/java/Lists/SubsetsII.java
@@ -2,8 +2,7 @@ package Lists;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @description: 描述 Medium
@@ -35,14 +34,43 @@ public class SubsetsII {
     }
 
     /**
-     *
+     * Runtime: 10 ms, faster than 12.57% of Java online submissions for Subsets II.
      * 递归解决
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> subsetsWithDup2(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(new ArrayList<>());
+        Arrays.sort(nums);
+        Set<String> set = new HashSet<>();
+        for (int i = 1; i <= nums.length; i++) {
+            helper(ans,i,0,new ArrayList<>(),nums,set);
+        }
+        return ans;
+    }
+    public void helper(List<List<Integer>> ans,int len,int start,List<Integer> curlist,int[] nums,Set<String> set){
+        if(len == 0){
+            if(set.add(curlist.toString()))
+            ans.add(new ArrayList<>(curlist));
+        }else{
+            for (int i = start; i < nums.length; i++) {
+                curlist.add(nums[i]);
+                helper(ans,len-1,i+1,curlist,nums,set);
+                curlist.remove(curlist.size()-1);
+            }
+        }
+    }
+
+    /**
+     * Runtime: 2 ms, faster than 98.79% of Java online submissions for Subsets II.
      * @param nums
      * @return
      */
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         ans.add(new ArrayList<>());
+        Arrays.sort(nums);
         for (int i = 1; i <= nums.length; i++) {
             helper(ans,i,0,new ArrayList<>(),nums);
         }
@@ -53,9 +81,11 @@ public class SubsetsII {
             ans.add(new ArrayList<>(curlist));
         }else{
             for (int i = start; i < nums.length; i++) {
+                if(i>start&&nums[i]==nums[i-1]) continue;//2ms
                 curlist.add(nums[i]);
                 helper(ans,len-1,i+1,curlist,nums);
                 curlist.remove(curlist.size()-1);
+//                while (i + 1 < nums.length && nums[i] == nums[i + 1]) ++i;  4ms
             }
         }
     }
