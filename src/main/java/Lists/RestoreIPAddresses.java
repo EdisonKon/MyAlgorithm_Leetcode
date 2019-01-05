0e5ced81625f0f0694e4eeb1694ef80e02310a03
EdisonKon <huainian_cl@126.com>
2019-01-05 15:50:49 +0800

    leetcode 93 Medium

---
+++ src/main/java/Lists/RestoreIPAddresses.java
@@ -0,0 +1,34 @@
+package Lists;
+
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @description: 描述 Medium
+ * @author: dekai.kong
+ * @date: 2019-01-05 15:50
+ * @from https://leetcode.com/problems/restore-ip-addresses/
+ * Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+ *
+ * Example:
+ *
+ * Input: "25525511135"
+ * Output: ["255.255.11.135", "255.255.111.35"]
+ */
+
+public class RestoreIPAddresses {
+    public RestoreIPAddresses() {
+
+    }
+
+    public List<String> restoreIpAddresses(String s) {
+        return null;
+    }
+
+    @Test
+    public void test() {
+
+    }
+}
+
