cfa35730367c0ea5d0e89eb46052735c87a618e0
dekai.kong <huainian_cl@126.com>
2019-01-06 19:26:18 +0800

    leetcode 93 medium

--- src/main/java/Lists/RestoreIPAddresses.java
+++ src/main/java/Lists/RestoreIPAddresses.java
@@ -2,6 +2,7 @@ package Lists;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -23,7 +24,21 @@ public class RestoreIPAddresses {
     }
 
     public List<String> restoreIpAddresses(String s) {
-        return null;
+        List<String> res = new ArrayList<>();
+        helper(s, 0, "", res);
+        return res;
+    }
+    public void helper(String s, int n, String out, List<String> res) {
+        if (n == 4) {
+            if (s.isEmpty()) res.add(out);
+            return;
+        }
+        for (int k = 1; k < 4; ++k) {
+            if (s.length() < k) break;
+            int val = Integer.parseInt(s.substring(0, k));
+            if (val > 255 || k != String.valueOf(val).length()) continue;
+            helper(s.substring(k), n + 1, out + s.substring(0, k) + (n == 3 ? "" : "."), res);
+        }
     }
 
     @Test
