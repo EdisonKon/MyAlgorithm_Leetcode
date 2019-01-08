59f90a13a7fe0dcb622f0b503f91c4dee59f1e12
EdisonKon <huainian_cl@126.com>
2019-01-08 11:22:32 +0800

    leetcode treenode

---
+++ src/main/java/Entitys/TreeNode.java
@@ -0,0 +1,28 @@
+package Entitys;
+
+import org.junit.Test;
+
+/**
+ * @description: 描述
+ * @author: dekai.kong
+ * @date: 2019-01-08 11:21
+ * @from
+ */
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+
+
+
+    @Test
+    public void test() {
+
+    }
+}
+
