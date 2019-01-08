90c684faf58be8aba7f6d9f6fac470ec1c34270c
EdisonKon <huainian_cl@126.com>
2019-01-08 11:18:30 +0800

    leetcode 93 Medium

--- src/main/java/Lists/RestoreIPAddresses.java
+++ src/main/java/Lists/RestoreIPAddresses.java
@@ -17,8 +17,8 @@ import java.util.List;
  * Input: "25525511135"
  * Output: ["255.255.11.135", "255.255.111.35"]
  * 一是只要遇到字符串的子序列或配准问题首先考虑动态规划DP，二是只要遇到需要求出所有可能情况首先考虑用递归。
- * 用一个n记录.的个数
- * TODO
+ * 用一个k记录当前的 数字 个数,用一个n记录当前 . 的个数
+ * TODO 递归,可以多刷 从a.a.a.a -->a.a.a.aa--> a.a.a.aaa --> a.a.aa.a--> a.a.aa.aa-->a.a.aa.aaa->.....等等递归
  */
 
 public class RestoreIPAddresses {
@@ -26,6 +26,11 @@ public class RestoreIPAddresses {
 
     }
 
+    /**
+     * Runtime: 4 ms, faster than 39.58% of Java online submissions for Restore IP Addresses.
+     * @param s
+     * @return
+     */
     public List<String> restoreIpAddresses(String s) {
         List<String> res = new ArrayList<>();
         helper(s, 0, "", res);
@@ -47,7 +52,7 @@ public class RestoreIPAddresses {
 
     @Test
     public void test() {
-
+        System.out.println(restoreIpAddresses("25525511135"));
     }
 }
 
