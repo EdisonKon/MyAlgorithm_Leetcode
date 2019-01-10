a83356f643dc4f5615f7839339e9b2e0f55ff43b
EdisonKon <huainian_cl@126.com>
2019-01-10 11:09:02 +0800

    leetcode 96 Medium

---
+++ src/main/java/Integers/UniqueBinarySearchTrees.java
@@ -0,0 +1,25 @@
+package Integers;
+
+import org.junit.Test;
+
+/**
+ * @description: 描述 Medium  96. Unique Binary Search Trees
+ * @author: dekai.kong
+ * @date: 2019-01-10 11:08
+ * @from
+ */
+
+public class UniqueBinarySearchTrees {
+    public UniqueBinarySearchTrees() {
+
+    }
+
+    public int numTrees(int n) {
+    }
+
+    @Test
+    public void test() {
+
+    }
+}
+
