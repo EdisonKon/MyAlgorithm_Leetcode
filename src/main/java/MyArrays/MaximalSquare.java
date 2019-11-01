998c935254de9cd432797ca0e5c23eda75c21fb7
EdisonKon <huainian_cl@126.com>
2019-11-01 16:14:49 +0800

    leetcode 221 medium https://leetcode.com/problems/maximal-square/submissions/

--- src/main/java/MyArrays/MaximalSquare.java
+++ src/main/java/MyArrays/MaximalSquare.java
@@ -8,7 +8,20 @@ import zUtils.ForMatrixArrays;
  * @author dekai.kong
  * @difficult Medium
  * @create 2019-11-01 15:12
- * @from
+ * @from https://leetcode.com/problems/maximal-square/submissions/
+ *
+ * Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
+ *
+ * Example:
+ *
+ * Input:
+ *
+ * 1 0 1 0 0
+ * 1 0 1 1 1
+ * 1 1 1 1 1
+ * 1 0 0 1 0
+ *
+ * Output: 4
  **/
 public class MaximalSquare {
     public MaximalSquare() {
