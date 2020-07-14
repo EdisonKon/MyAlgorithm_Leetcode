01ad9491c80a936df1ae1e18b6ccca866a81fb14
EdisonKon <huainian_cl@126.com>
2020-07-14 11:00:46 +0800

    镜像二叉树

--- src/main/java/BinaryTree/IsSymmetric.java
+++ src/main/java/BinaryTree/IsSymmetric.java
@@ -83,7 +83,7 @@ public class IsSymmetric {
 
     private boolean helper(TreeNode left, TreeNode right) {
         if(left==null&&right==null) return true;
-        if(left.val != right.val || left==null || right==null){
+        if(left==null || right==null||left.val != right.val){
             return false;
         }
         return helper(left.left,right.right) && helper(left.right,right.left);
