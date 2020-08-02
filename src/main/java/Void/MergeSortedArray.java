8dcef24478728f61ebc79f3121d0373d67bb1e64
EdisonKon <huainian_cl@126.com>
2020-08-02 21:37:28 +0800

    合并两个有序数组

--- src/main/java/Void/MergeSortedArray.java
+++ src/main/java/Void/MergeSortedArray.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
  * @description: 描述 Easy
  * @author: dekai.kong
  * @date: 2018-12-28 16:20
- * @from https://leetcode.com/problems/merge-sorted-array/
+ * @from 88. https://leetcode.com/problems/merge-sorted-array/
  * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
  *
  * Note:
@@ -66,9 +66,33 @@ public class MergeSortedArray {
         }
     }
 
+    /**
+     * 从后往前填充数组,且最后只处理n不为0的情况即可 0ms 100%
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public void mergex(int[] nums1, int m, int[] nums2, int n) {
+        while(m>0 && n>0){
+            if(nums1[m-1]>nums2[n-1]){
+                nums1[m+n-1] = nums1[m-1];
+                m--;
+            }else{
+                nums1[m+n-1] = nums2[n-1];
+                n--;
+            }
+        }
+
+        while(n>0){
+            nums1[m+n-1] = nums2[n-1];
+            n--;
+        }
+    }
+
     @Test
     public void test() {
-        merge(new int[]{1,2,3,0,0,0},3,new int[]{2,5,6},3);
+        mergex(new int[]{1,2,3,0,0,0},3,new int[]{2,5,6},3);
     }
 }
 
